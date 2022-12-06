913608d7e3a51d6735547c187646c88e5036b9fe
karanmaurya15 <karanmaurya9663@gmail.com>
2022-12-06 14:24:27 +0530

    Wellcome

---
+++ DataStructure-LinkedList/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DataStructure_LinkedList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wellcome to Data Structure Program");
+        }
+    }
+}
